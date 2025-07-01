0266c97ecc3e562770167691b0ce6325d02bcd39
Tfive <alimanutd10r@gmail.com>
2025-07-01 05:42:06 +0330

    print info

--- src/client.c
+++ src/client.c
@@ -85,6 +85,8 @@ int main(int argc, char *argv[])
     return -1;
   }
 
+  printf("Connecting to %s:%d\n", config.ip, config.port);
+
   if ((sock_fd = sock_connect(AF_INET, SOCK_STREAM, 0, config.ip, config.port)) < 0)
   {
     perror("sock_connect()");
