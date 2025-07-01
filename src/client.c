24e1a632def5f59c050e52fe77ee8899cf8ee14d
Tfive <alimanutd10r@gmail.com>
2025-07-01 04:18:45 +0330

    a bit too high

--- src/client.c
+++ src/client.c
@@ -59,8 +59,6 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  while(1);
-
   if ((sock_fd = sock_connect(AF_INET, SOCK_STREAM, 0, config.ip, config.port)) < 0)
   {
     perror("sock_connect()");
